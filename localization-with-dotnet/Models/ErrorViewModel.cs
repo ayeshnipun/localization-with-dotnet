5d63620c7a16e45c2d93fbb68348478b6476a4e9
ayeshNipun <nipun.yesh@gmail.com>
2021-09-05 15:56:32 +0530

    Add project files.

---
+++ localization-with-dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace localization_with_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
